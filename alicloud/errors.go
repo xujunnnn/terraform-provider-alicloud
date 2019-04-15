59b5f7eb3bacce9ba0401a92aac3de3210ce26c0
xujunnnn <vega.xj@alibaba-inc.com>
2019-04-15 11:01:31 +0800

    添加重试机制

--- alicloud/errors.go
+++ alicloud/errors.go
@@ -57,8 +57,8 @@ const (
 	SlbAclEntryEmpty                    = "AclEntryEmpty"
 	SlbAclNameExist                     = "AclNameExist"
 	SlbTokenIsProcessing                = "OperationFailed.TokenIsProcessing"
-
-	SlbCACertificateIdNotFound = "CACertificateId.NotFound"
+	SlbCommodityCodeNotExists           = "CommodityCodeNotExists"
+	SlbCACertificateIdNotFound          = "CACertificateId.NotFound"
 	// slb server certificate
 	SlbServerCertificateIdNotFound = "ServerCertificateId.NotFound"
 
